1b5654983bd5b3db4a374b4334b0ca806e448eff
Alejandra Cáceres <alejandraicaceres@gmail.com>
2023-05-25 08:21:24 -0300

    Add axios and request to database service for all microservices

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -1,12 +1,13 @@
-const characters = require("./characters.json")
+const axios = require("axios")
 
 module.exports = {
   list: async () => {
-    return characters
+    const results = await axios.get("http://database:8004/Character")
+    return results.data
   },
 
   create: async () => {
-    throw Error("error")
+    throw Error("Error on load characters from DB")
   }
 
 }
--- films/src/data/index.js
+++ films/src/data/index.js
@@ -1,12 +1,13 @@
-const films = require("./films.json")
+const axios = require("axios")
 
 module.exports = {
   list: async () => {
-    return films
+    const results = await axios.get("http://database:8004/Film")
+    return results.data
   },
 
   create: async () => {
-    throw Error("error")
+    throw Error("Error on load films from DB")
   }
 
 }
